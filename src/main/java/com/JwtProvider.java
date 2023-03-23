cb17b723f85a0ada121f28703dbd9d8a9f61fc35
tagoner <ddamda0101@gmail.com>
2023-03-23 16:54:34 +0900

    JWT 작업 Junit 테스트 파일구조 변경

---
+++ src/main/java/com/JwtProvider.java
@@ -0,0 +1,57 @@
+package com;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+
+@Component(value="Jwt")
+public class JwtProvider {
+	
+    @Value("${api.key}")
+    private String KEY;
+    
+	//만기시간 1000초
+	private static long vaildTime = 1000L * 100000;
+	
+	
+	//JWT 토큰 생성
+	public String createToken(Map<String, String> payload) {
+		Claims claims = Jwts.claims();
+		claims.putAll(payload);
+		
+		Date current = new Date();
+		return Jwts.builder()
+		           //Payload
+		           .setClaims(claims)
+		           //발행시간
+		           .setIssuedAt(current)
+		           //만기시간
+		           .setExpiration(new Date(current.getTime() + vaildTime))
+		           .signWith(getEncriptKey(KEY), SignatureAlgorithm.HS256)
+		           .compact();
+	}
+	
+	//Key HMAC_SHA 인코딩
+	private Key getEncriptKey(String secretKey) {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+	
+	//JWT 토큰 조회
+	public Claims getClaims(String token) {
+		return Jwts.parserBuilder()
+		           .setSigningKey(getEncriptKey(KEY))
+		           .build()
+		           .parseClaimsJws(token)
+		           .getBody();
+	}
+}
---
+++ src/test/java/Calulator.java
@@ -0,0 +1,43 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.JwtProvider;
+import com.core.Application;
+
+import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@ContextConfiguration(classes = Application.class)
+@SpringBootTest
+class Calulator {
+	
+    @Resource(name="Jwt")
+    private JwtProvider test;
+    
+    
+	@Test
+	void test() {
+		Map<String,String> param = new LinkedHashMap<>();
+		param.put("name", "tagon");
+		param.put("age", "29");
+		
+		try {
+    		String token = test.createToken(param);
+    		log.info("en_token : {}", token);
+    		
+    		Claims claims = test.getClaims(token);
+    		log.info("de_token : {}", claims);
+		}
+		catch(Exception e) {
+		    log.error("", e);
+		}
+	}
+}
